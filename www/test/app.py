010ffc1843aa232618ece8efcab5034a4bb765fd
baiyan <12345678>
2018-12-28 11:15:26 +0800

    day2 修复报错

--- www/test/app.py
+++ www/test/app.py
@@ -1,21 +1,16 @@
 import logging; logging.basicConfig(level=logging.INFO)
 
-import asyncio, os, json, time
-from datetime import datetime
-
 from aiohttp import web
 
 def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+    return web.Response(body='<h1>AWesome</h1>'.encode('utf-8'), content_type='text/html')
 
-@asyncio.coroutine
-def init(loop):
-    app = web.Application(loop=loop)
+
+def init():
+    app = web.Application()
     app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    web.run_app(app, host='133.193.89.25', port=9000)
     logging.info('server started at http://127.0.0.1:9000...')
-    return srv
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
-loop.run_forever()
+
+init()
